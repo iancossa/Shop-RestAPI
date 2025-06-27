42dc348f703f0ee11fb0425882080c100edf264a
iancossa <ianrafaelelias@gmail.com>
2025-06-27 22:01:36 +0530

    Start testing the requests

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
 
+
+const productRoutes = require('./api/routes/products');//Importing the product routes
 app.use((req, res, next) => {
     res.status(200).json({
         message: 'It works!'
     });
 });
+app.use('/products', productRoutes); //Using the product routes 
 
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const port = process.env.PORT || 3000;
 const app = require('./app'); // Import the app from app.js
-const server = http.createServer();
+const server = http.createServer(app);
 
 server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
